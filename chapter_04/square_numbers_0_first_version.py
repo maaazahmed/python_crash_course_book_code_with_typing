95612b461c64c044a42cf2e87006d99e41a8454b
maaazahmed <maazahmeed2k16@gmail.com>
2024-06-13 23:53:14 +0500

    chapter-06 => dictionary

--- chapter_04/square_numbers_0_first_version.py
+++ chapter_04/square_numbers_0_first_version.py
@@ -6,3 +6,15 @@ for value in range(1,11):
     squares.append(square)
 
 print(squares)
+
+
+from typing import Union
+
+
+
+alien_0:dict[str, Union[str, int]] = {'color': 'green', 'points': 5}
+alien_1:dict[str, Union[str, int]] = {'color': 'yellow', 'points': 10}
+alien_2:dict[str, Union[str, int]] = {'color': 'red', 'points': 15}
+
+
+aliens:list[dict[str, Union[str, int]]] = [alien_0, alien_1, alien_2]
--- chapter_04/square_numbers_1_shorter_loop.py
+++ chapter_04/square_numbers_1_shorter_loop.py
@@ -3,3 +3,21 @@ for value in range(1, 11):
     squares.append(value**2)
 
 print(squares)
+
+from typing import Union
+
+# alien_0:dict[str, Union[str, int]] = {"color":"green", "points":5}
+
+# print(alien_0["color"])
+# print(alien_0["points"])
+
+
+
+# Typss = dict[str, Union[str, int]]
+
+# alien_0:Typss = {'color': 'green', 'points': 5}
+# alien_1:Typss = {'color': 'yellow', 'points': 10}
+# alien_2:Typss = {'color': 'red', 'points': 15}
+
+
+# aliens:list[Typss] = [alien_0, alien_1, alien_2]
